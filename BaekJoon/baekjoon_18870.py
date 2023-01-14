68334769af95c12e1684d3f87d3be0c74be29f0b
heesane <tees3359@gmail.com>
2023-01-15 03:34:00 +0900

    CodingStudy

--- BaekJoon/baekjoon_18870.py
+++ BaekJoon/baekjoon_18870.py
@@ -1,10 +1,7 @@
 import sys
-
 input = sys.stdin.readline
-
-n = int(input())
+N = int(input())
 arr = list(map(int, input().split()))
-
 arr2 = sorted(list(set(arr)))
 dic = {arr2[i] : i for i in range(len(arr2))}
 for i in arr:
